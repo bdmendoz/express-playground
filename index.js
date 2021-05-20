c5e20c4f9e81de437b633d505e65ae1e3b37bb0e
Bayron Daniel Mendoza <bayrondmendoza@gmail.com>
2021-05-19 21:39:21 -0500

    final crud adjustments without any architecture

--- index.js
+++ index.js
@@ -1,40 +1,168 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-const AWS= require('aws-sdk');
-const config= require('./config');
+const AWS = require('aws-sdk');
+const config = require('./config');
+const { v4: uuidv4 } = require('uuid');
+const e = require('express');
+
+app.use(express.json());
+
+AWS.config.update(config.aws_remote_config);
 
 
+app.get('/', function (req, res, next) {
+    res.send({ hello: 'hola ' });
+});
+app.get('/trabajadores', function (req, res) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
 
-
-app.get('/trabajadores', function(req, res, next){
-    AWS.config.update(config.aws_remote_config);
-    console.log(config.aws_remote_config);
-    const docClient= new AWS.DynamoDB.DocumentClient();
-    
-    const params= {
+    const params = {
         TableName: config.aws_table_name
     }
-    docClient.scan(params, function(err, data){
-        if(err){
-            console.log(err);
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            
             res.send({
-                    success:false,
-                    message: err
-                });
-        }else{
-            console.log(data);
-            const {Items} = data;
+                success: false,
+                message: err
+            });
+        } else {
+            
+            const { Items } = data;
             res.send({
-                    success:true,
-                    trabajadores: Items
+                success: true,
+                trabajadores: Items
             });
         }
-        
+
+    });
+});
+app.get('/trabajadores/:EmployeeId', function (req, res) {
+    const { EmployeeId } = req.params;
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: config.aws_table_name,
+        Key: {
+            id: EmployeeId
+        }
+    };
+    docClient.get(params, function (err, data) {
+        if (err) {
+            
+            res.send({
+                success: false,
+                message: err
+            });
+        } else {
+            const {Item} = data;
+            res.send({
+                success: true,
+                data: Item
+            });
+        }
+    });
+});
+app.post('/trabajadores', function (req, res) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const item = { ...req.body };
+    item.id = uuidv4();
+    const params = {
+        TableName: config.aws_table_name,
+        Item: item
+    };
+
+    docClient.put(params, function (err, data) {
+        if (err) {
+            res.send({
+                success: false,
+                message: err
+            });
+        } else {
+            
+            res.send(
+                {
+                    success: true,
+                    message: 'Added item',
+                    data: item
+                }
+            );
+        }
+    });
+
+});
+
+app.put('/trabajadores/:EmployeeId', function (req, res) {
+    const item = { ...req.body };
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const { EmployeeId } = req.params;
+
+    
+    const params = {
+        TableName: config.aws_table_name,
+        Key: {
+            id: EmployeeId
+        },
+        UpdateExpression: "set cargo = :c, celular=:p, edad=:e, nombre=:n",
+        ExpressionAttributeValues: {
+            ":c": item.cargo,
+            ":p": item.celular,
+            ":e": item.edad,
+            ":n": item.nombre
+        },
+        ReturnValues: "ALL_NEW"
+    };
+    
+    docClient.update(params, function (err, data) {
+        if (err) {
+            res.send({
+                success: false,
+                message: err
+            });
+        } else {
+            const {Attributes}= data;
+            res.send(
+                {
+                    success: true,
+                    message: 'Updated item',
+                    data: Attributes
+                }
+            );
+        }
+    });
+});
+app.delete('/trabajadores/:EmployeeId', function (req, res, next) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const { EmployeeId } = req.params;
+    
+    const params = {
+        TableName: config.aws_table_name,
+        Key: {
+            id: EmployeeId
+
+        },
+        ReturnValues: "ALL_OLD"
+    };
+     
+    docClient.delete(params, function (err, data) {
+        if (err) {
+            res.send({
+                success: false,
+                message: err
+            });
+
+        } else {
+            res.send(
+                {
+                    success: true,
+                    message: 'deleted item',
+                    data: data
+                }
+            );
+        }
     });
 });
 
-
-const server = app.listen(3001, function(){
+const server = app.listen(3001, function () {
     console.log(`Listening at http://localhost:${server.address().port}/`)
 })
 
